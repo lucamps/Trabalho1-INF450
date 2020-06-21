5003f34040bd545af4599c69a5c070d2867fd73b
Matheus L <30654908+MatheusAL@users.noreply.github.com>
2020-06-21 20:43:49 -0300

    Criando arquivo em C

---
+++ Etapa1/somatorio.c
@@ -0,0 +1,13 @@
+int somatorio(int soma){
+    int N = 100000;
+    int a[N];
+    int i=0;
+    for(i; i < N; i++){
+        if(a[i] > 0 ){
+            soma+= a[i];
+        }
+        else{ 
+            soma = soma - a[i];
+        }
+    }   
+}
